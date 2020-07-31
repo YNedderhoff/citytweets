dc98ddeefa32191135a83700730b2b3f7824a5ad
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2020-07-31 18:43:04 +0200

    Set up Spring Boot

---
+++ src/main/java/xyz/nedderhoff/citytweets/CityTweetsApplication.java
@@ -0,0 +1,13 @@
+package xyz.nedderhoff.citytweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTweetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/nedderhoff/citytweets/CityTweetsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nedderhoff.citytweets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTweetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
