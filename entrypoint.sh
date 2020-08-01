1a3e0634d1de7cc2f0f5f412bf913dfe561cd22b
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2020-08-02 01:16:21 +0200

    fix issues with double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 exec /opt/java/openjdk/bin/java -Dinst.type=citytweets \
-    -Dcitytweets.inst.id="$HOSTNAME" \
-    "${JVM_PARAMETERS:-}" \
-    "${JAVA_OPTS:-}" \
-    "${JAVA_OPTS_GC:-}" \
-    "${JAVA_OPTS_MEM:-}" \
-    "${JAVA_OPTS_DEBUG:-}" \
+    -Dcitytweets.inst.id=$HOSTNAME \
+    ${JVM_PARAMETERS:-} \
+    ${JAVA_OPTS:-} \
+    ${JAVA_OPTS_GC:-} \
+    ${JAVA_OPTS_MEM:-} \
+    ${JAVA_OPTS_DEBUG:-} \
     -XX:OnOutOfMemoryError='kill -9 %p' \
     -XX:+HeapDumpOnOutOfMemoryError \
     -XX:HeapDumpPath=/dumps/citytweets.bin \
