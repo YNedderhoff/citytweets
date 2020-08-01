a38d9be4ed54865394240194191f011b56923802
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2020-08-01 17:40:50 +0200

    Setup up basic Spring Boot app

--- src/main/java/xyz/nedderhoff/citytweets/CityTweetsApplication.java
+++ src/main/java/xyz/nedderhoff/citytweets/CityTweetsApplication.java
@@ -2,12 +2,21 @@ package xyz.nedderhoff.citytweets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 @SpringBootApplication
 public class CityTweetsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CityTweetsApplication.class, args);
+	}
+
+	@Bean
+	public Twitter twitter(){
+		return new TwitterFactory().getInstance();
 	}
 
 }
