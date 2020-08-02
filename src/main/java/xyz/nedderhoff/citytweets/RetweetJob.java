631bf84f13b535675f91fc7fec50a0c69a258be2
Yanick Nedderhoff <yanicknedderhoff@gmail.com>
2020-08-02 02:35:01 +0200

    ignore own account when retweeting

--- src/main/java/xyz/nedderhoff/citytweets/RetweetJob.java
+++ src/main/java/xyz/nedderhoff/citytweets/RetweetJob.java
@@ -23,14 +23,17 @@ public class RetweetJob {
     private final Twitter twitter;
     private final RetweetCache retweetCache;
     private final String search;
+    private final String ownHandle;
 
     @Autowired
     public RetweetJob(Twitter twitter,
                       RetweetCache retweetCache,
-                      @Value("${search}") String search) {
+                      @Value("${search}") String search,
+                      @Value("${own-handle}") String ownHandle) {
         this.twitter = twitter;
         this.retweetCache = retweetCache;
         this.search = search;
+        this.ownHandle = ownHandle;
     }
 
     @Scheduled(fixedRate = FETCHING_RATE)
@@ -42,6 +45,7 @@ public class RetweetJob {
         QueryResult result = twitter.search(query);
 
         result.getTweets().stream()
+                .filter(tweet -> !tweet.getUser().getName().toLowerCase().equals(ownHandle.toLowerCase()))
                 .filter(tweet -> !retweetCache.contains(tweet.getId()))
                 .filter(tweet -> !tweet.getText().startsWith("RT @"))
                 .peek(tweet -> logger.info("Found Tweet: ID \"{}\", Author \"{}\", Language \"{}\", Location \"{}\", Text \"{}\".",
