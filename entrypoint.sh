4c8bbba1c95afa48a7703fe5c07f5e006639ec96
ynedderhoff <hey@yanick.xyz>
2021-04-11 00:00:34 +0200

    ignore shellcheck SC2086

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Ignoring shellcheck SC2086 errors. It would otherwise complain about missing quotes:
+#
+# exec java ${JVM_PARAMETERS:-} \
+#           ^-----------------^ SC2086: Double quote to prevent globbing and word splitting.
+#
+# However we actually want word splitting, so that java arguments such as -Xone -Xtwo -Xthree are actually passed as
+# separate arguments, an not one string, i.e. "-Xone -Xtwo -Xthree".
+#
+# shellcheck disable=SC2086
+#
+
 exec /opt/java/openjdk/bin/java -Dinst.type=citytweets \
     -Dcitytweets.inst.id=$HOSTNAME \
     ${JVM_PARAMETERS:-} \
