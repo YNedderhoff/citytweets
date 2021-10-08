e3d12f4d5ff362065d3def72637a70ee164358f4
ynedderhoff <hey@yanick.xyz>
2021-10-08 23:07:44 +0200

    make TwitterApi1Endpoint deprecated forRemoval

--- src/main/java/xyz/nedderhoff/citytweets/twitter/TwitterApi1Endpoint.java
+++ src/main/java/xyz/nedderhoff/citytweets/twitter/TwitterApi1Endpoint.java
@@ -2,7 +2,7 @@ package xyz.nedderhoff.citytweets.twitter;
 
 import org.springframework.web.client.RestTemplate;
 
-@Deprecated
+@Deprecated(forRemoval = true)
 // https://developer.twitter.com/en/docs/twitter-api/v1
 public abstract non-sealed class TwitterApi1Endpoint extends TwitterHttpEndpoint {
     protected static final String BASE_TWITTER_API_1_URI = BASE_TWITTER_API_URI + "1.1/";
