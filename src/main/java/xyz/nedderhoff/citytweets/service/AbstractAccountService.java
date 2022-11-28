a444df409075f67ff228fb66c6a3bf4ba6aa027d
ynedderhoff <hey@yanick.xyz>
2022-11-28 21:07:11 +0100

    log ignored accounts

--- src/main/java/xyz/nedderhoff/citytweets/service/AbstractAccountService.java
+++ src/main/java/xyz/nedderhoff/citytweets/service/AbstractAccountService.java
@@ -1,12 +1,15 @@
 package xyz.nedderhoff.citytweets.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import xyz.nedderhoff.citytweets.config.AccountProperties.Account;
 import xyz.nedderhoff.citytweets.config.AccountProperties.PlatformProperties;
 
 import java.util.List;
 
 public abstract class AbstractAccountService<K extends Account, T extends PlatformProperties<K>>
-        implements AccountService<K>{
+        implements AccountService<K> {
+    private static final Logger logger = LoggerFactory.getLogger(AbstractAccountService.class);
 
     protected final List<K> accounts;
     protected final List<String> ignoredAccounts;
@@ -14,6 +17,14 @@ public abstract class AbstractAccountService<K extends Account, T extends Platfo
     public AbstractAccountService(T properties) {
         this.accounts = properties.accounts();
         this.ignoredAccounts = properties.ignoredAccounts();
+
+        logger.info("Found {} globally ignored {} accounts", ignoredAccounts.size(), properties.getClass().getSimpleName());
+        accounts.forEach(account -> logger.info(
+                "Found {} ignored accounts for {} {}",
+                account.ignoredAccounts().size(),
+                account.getClass().getSimpleName(),
+                account.name()
+        ));
     }
 
     @Override
