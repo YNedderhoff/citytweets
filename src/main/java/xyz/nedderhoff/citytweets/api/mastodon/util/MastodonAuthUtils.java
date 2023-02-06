dd429639f1c1eb37aa1b015c770a9472c4429112
ynedderhoff <hey@yanick.xyz>
2023-02-06 09:26:21 +0100

    remove unused logger

--- src/main/java/xyz/nedderhoff/citytweets/api/mastodon/util/MastodonAuthUtils.java
+++ src/main/java/xyz/nedderhoff/citytweets/api/mastodon/util/MastodonAuthUtils.java
@@ -1,13 +1,10 @@
 package xyz.nedderhoff.citytweets.api.mastodon.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import xyz.nedderhoff.citytweets.config.AccountProperties.MastodonAccount;
 
 public class MastodonAuthUtils {
-    private static final Logger logger = LoggerFactory.getLogger(MastodonAuthUtils.class);
 
     public static HttpHeaders getHttpHeadersWithAuth(MastodonAccount mastodonAccount) {
         final HttpHeaders authedHeaders = new HttpHeaders();
