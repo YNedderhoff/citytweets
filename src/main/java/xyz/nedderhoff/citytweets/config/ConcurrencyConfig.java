79f5ccd920eee4508a578282df1bb07607a40d7f
ynedderhoff <hey@yanick.xyz>
2023-02-08 00:50:51 +0100

    introduce SchedulerConfig

--- src/main/java/xyz/nedderhoff/citytweets/config/ConcurrencyConfig.java
+++ src/main/java/xyz/nedderhoff/citytweets/config/ConcurrencyConfig.java
@@ -9,14 +9,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
-public class CityTweetsConfig {
+public class ConcurrencyConfig {
 
+    @Bean
     public ExecutorService repostJobExecutorService(List<RepostService> repostServices) {
         return Executors.newFixedThreadPool(repostServices.size());
-    }
-
-    @Bean(destroyMethod = "shutdown")
-    public ExecutorService taskScheduler() {
-        return Executors.newScheduledThreadPool(3);
     }
 }
