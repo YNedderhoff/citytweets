2a4d6b003fa0404ba34d8d96307ad8602fa6cf70
ynedderhoff <hey@yanick.xyz>
2023-02-08 01:57:11 +0100

    make SchedulerConfig @Configuration

--- src/main/java/xyz/nedderhoff/citytweets/config/SchedulerConfig.java
+++ src/main/java/xyz/nedderhoff/citytweets/config/SchedulerConfig.java
@@ -1,9 +1,11 @@
 package xyz.nedderhoff.citytweets.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 
     @Override
